7f4eea7fa254ff9836d4cec79f7d26faf012eedc
lbr1014 <lbr1014@alu.ubu.es>
2026-01-08 15:07:43 +0100

    prototipo del loging y singup de la página

---
+++ prototipos/Flask/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Contraseña", validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField("Iniciar sesión")
+
+class SignupForm(FlaskForm):
+    nombre = StringField("Nombre", validators=[DataRequired(), Length(min=2, max=50)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Contraseña", validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField(
+        "Repite la contraseña",
+        validators=[DataRequired(), EqualTo("password", message="Las contraseñas no coinciden")]
+    )
+    submit = SubmitField("Crear cuenta")
--- prototipos/Flask/run.py
+++ prototipos/Flask/run.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+
+from forms import LoginForm, SignupForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "una_clave_cualquiera_118732hfshdfiuhwy!!$%"
 
 @app.route("/")
 def inicio():
@@ -9,6 +12,35 @@ def inicio():
         titulo="Implementación de un RAG sobre las licitacioens del estado",
         autor="Autora: Lydia Blanco Ruiz"
     )
+    
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        
+        return redirect(url_for("pag_principal"))
+        
+    return render_template("login.html", form=form)
+
+@app.route("/singup", methods=["GET", "POST"])
+def singup():
+    form = SignupForm()
+    
+    if form.validate_on_submit():
+        nombre = form.nombre.data
+        email = form.email.data
+        password = form.password.data
+        
+        return redirect(url_for("pag_principal"))
+    
+    return render_template("singup.html", form=form)
+
+@app.route("/pagina_principal", methods=["GET"])
+def pag_principal():
+    return render_template("pag_principal.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
