6814a4cfce0d78b8143c551994f5db3518252464
lbr1014 <lbr1014@alu.ubu.es>
2026-03-04 22:10:27 +0100

    Añadir usuario administardor por defecto para comenzar a usar la aplicaición en local

--- app/PythIA/migrations/versions/7b8a876a95ae_create_an_admin.py
+++ app/PythIA/migrations/versions/7b8a876a95ae_create_an_admin.py
@@ -17,25 +17,25 @@ depends_on = None
 
 
 def upgrade():
-    # Crear usuario admin Lydia
+    # Crear usuario admin 
     op.execute("""
     INSERT INTO users (nombre, email, password_hash, last_login, is_admin)
     SELECT
-        'Lydia',
-        'lydiab293@gmail.com',
+        'Admin',
+        'admin@gmail.com',
         'scrypt:32768:8:1$pl74Bt5pwnEUvxuX$3037992e1373b196b4e42fe4b56aea5410ef7c2602dc57a0686fd2d7b741c476f512441b39c4680c566c47b93205be9a6ebefd4db9341bdd07a9078547aa958f',
         NULL,
         TRUE
     WHERE NOT EXISTS (
-        SELECT 1 FROM users WHERE email = 'lydiab293@gmail.com'
+        SELECT 1 FROM users WHERE email = 'admin@gmail.com'
     );
     """)
 
-    # Si ya existía, asegúrate de que sea admin
+    # Si ya existía, assegurar que sea admin
     op.execute("""
     UPDATE users
     SET is_admin = True
-    WHERE email = 'lydiab293@gmail.com';
+    WHERE email = 'admin@gmail.com';
     """)
 
 
