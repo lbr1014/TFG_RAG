3cdb0f5433bbc015ff346af8737eed1350682cd3
lbr1014 <lbr1014@alu.ubu.es>
2026-03-09 13:43:16 +0100

    Hacer que el efecto luz no solo sirva para botones

--- app/PythIA/static/js/functionalities.js
+++ app/PythIA/static/js/functionalities.js
@@ -1,33 +1,16 @@
 /* =========================
-   DESPLEGABLE 
-   ========================= */
-document.addEventListener('click', function (e) {
-  const dropdowns = document.querySelectorAll('.dropdown');
-
-  dropdowns.forEach(dropdown => {
-    const button = dropdown.querySelector('.dropdown-toggle');
-
-    if (button && button.contains(e.target)) {
-      dropdown.classList.toggle('open');
-    } else {
-      dropdown.classList.remove('open');
-    }
-  });
-});
-
-/* =========================
    EFECTO LUZ 
    ========================= */
 
 document.addEventListener("mousemove", function (e) {
-  const btn = e.target.closest(".btn-modelo");
-  if (!btn) return;
+  const luz = e.target.closest(".luz");
+  if (!luz) return;
 
-  const rect = btn.getBoundingClientRect();
+  const rect = luz.getBoundingClientRect();
   const x = ((e.clientX - rect.left) / rect.width) * 100;
   const y = ((e.clientY - rect.top) / rect.height) * 100;
 
-  btn.style.setProperty("--x", `${x}%`);
-  btn.style.setProperty("--y", `${y}%`);
+  luz.style.setProperty("--x", `${x}%`);
+  luz.style.setProperty("--y", `${y}%`);
 });
 
